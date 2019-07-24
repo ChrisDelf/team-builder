12a64d5a44e92cdecca46985250f00dab1eaa46f
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-24 15:29:57 -0400

    initial commit

---
+++ team-builder/src/form.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+
+function TeamMemberBuilder() {
+
+  const [member, setMember] = useState ({memberName: "", role: "", email: ""});
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default TeamMemberBuilder ;
