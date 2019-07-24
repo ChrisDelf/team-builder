14128a7cc2727f38e01dbe3996b4fb67225ab84a
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-24 15:58:35 -0400

    form.js is working

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMemberBuilder from './form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> Hello
 
-      </header>
+    </header>
+    <TeamMemberBuilder/>
     </div>
   );
 }
--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,21 +1,67 @@
-import React, {useState} from "react";
-
+import React, { useState } from 'react';
 
 function TeamMemberBuilder() {
+  const [member, setMember] = useState({ username: '', role: '', email: '' });
 
-  const [member, setMember] = useState ({memberName: "", role: "", email: ""});
+  function changeHandler(event) {
+    const updatedUser = { ...member, [event.target.name]: event.target.value };
 
+    setMember(updatedUser);
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('memberName', member);
+  }
 
-
-
-
-
-
-
-
-
-
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Signup</legend>
+        <div className="form-group row">
+          <label>
+            MemberName
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Enter your username"
+                value={member.username}
+                onChange={changeHandler}
+              />
+            </div>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>Role</label>
+          <input
+            type="role"
+            className="form-control"
+            name="role"
+            aria-describedby="emailHelp"
+            placeholder="Enter role"
+            value={member.role}
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="emailHelp"
+            name="email"
+            placeholder="Enter Email"
+            onChange={changeHandler}
+            value={member.email}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </fieldset>
+    </form>
+  );
 }
 
-export default TeamMemberBuilder ;
+export default TeamMemberBuilder;
